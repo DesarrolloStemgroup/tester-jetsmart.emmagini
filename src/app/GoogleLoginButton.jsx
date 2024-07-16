56fcd7953267108eedd1b3d4253e18d3096d28e0
Lis <lisgimenaroa@gmail.com>
2024-07-16 12:10:03 -0300

    testeando login

--- src/app/GoogleLoginButton.jsx
+++ src/app/GoogleLoginButton.jsx
@@ -1,10 +1,19 @@
 // components/GoogleLoginButton.js
-import React from "react";
+
+"use client";
+
+import {
+	createContext,
+	useState,
+	useContext,
+	useEffect,
+	useCallback,
+} from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 
 const GoogleLoginButton = () => {
-	const handleGoogleLoginSuccess = async (credentialResponse) => {
+	/*const handleGoogleLoginSuccess = async (credentialResponse) => {
 		const data = {
 			host: "demo5.emmagini.com",
 			client_id: credentialResponse.clientId,
@@ -31,7 +40,36 @@ const GoogleLoginButton = () => {
 		} catch (error) {
 			console.error("Error logging in:", error);
 		}
-	};
+	}; */
+
+	const handleGoogleLoginSuccess = useCallback(async (credentialResponse) => {
+		try {
+			const response = await axios.post(
+				"https://backend.emmagini.com/api2/google_login",
+				{
+					host: "demo5.emmagini.com",
+					client_id: credentialResponse.clientId,
+					credential: credentialResponse.credential,
+					fcm_token: "",
+					es_app: "0",
+					id_plataforma: "3",
+					lang: "es",
+					timezone: "-3",
+				},
+				{
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+					},
+				}
+			);
+
+			console.log("Login successful:", response.data);
+		} catch (error) {
+			console.error("Error al hacer la solicitud:", error);
+
+			throw error;
+		}
+	}, []);
 
 	return (
 		<div className="flex flex-row gap-3 items-center justify-center rounded-full shadow-md border border-gray-200 cursor-pointer w-full">
